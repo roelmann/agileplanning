bd74c8ea4dffe4411438ebad48cc7325a87f1c1b
roglos <roelmann@glos.ac.uk>
2018-01-08 08:03:56 +0000

    Add IE content editable bug warning to Beta header

--- includes/navbar.php
+++ includes/navbar.php
@@ -77,4 +77,4 @@ ini_set("display_errors", 1);
     </ul>
   </div>
 </nav>
-<p class="alert alert-danger" style="margin-top:60px"><strong>THIS IS BETA FOR TESTING, COMMENT AND FURTHER DEVELOPMENT</strong><br>Tested in Chrome<br>Unknown Bug in Firefox means contenteditable tables are unreliable for integer values (treating as string and preventing database save???)<br>Untested in IE, but known bug in IE means ContentEditable is not supported in tables and will need to be reworked</p>
+<p class="alert alert-danger" style="margin-top:60px"><strong>THIS IS BETA FOR TESTING, COMMENT AND FURTHER DEVELOPMENT</strong><br>Tested in Chrome **<strong>Please use Chrome for any content editing in the Beta version</strong><br>Unknown Bug in Firefox means contenteditable tables are unreliable for integer values (treating as string and preventing database save???)<br>Untested in IE, but known <a href="https://caniuse.com/#search=contenteditable">issue in IE means ContentEditable is not supported in tables</a> and will need to be reworked</p>
